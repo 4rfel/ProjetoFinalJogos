3589dc7fc086e7bd4f2710f40e6e139664f50400
4rfel <38050598+4rfel@users.noreply.github.com>
2021-05-14 14:59:22 -0300

    online base

---
+++ Assets/_Scripts/Menu/MenuButtons.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using MLAPI;
+using MLAPI.SceneManagement;
+
+public class MenuButtons : NetworkBehaviour {
+
+	[SerializeField] GameObject buttons;
+	//[SerializeField] GameObject loading;
+	MLAPI.Transports.PhotonRealtime.PhotonRealtimeTransport transport;
+
+
+	private void Start() {
+		//transport = GameObject.FindGameObjectWithTag("NetworkManager").GetComponent<MLAPI.Transports.PhotonRealtime.PhotonRealtimeTransport>();
+	}
+
+	public void Host() {
+		//string str = "";
+		//for(int i = 0; i < 6; i++) {
+		//	str += (char) Random.Range(65, 90);
+		//}
+		//transport.RoomName = str;
+		NetworkManager.Singleton.StartHost();
+		NetworkSceneManager.SwitchScene("Loby");
+	}
+
+	public void Join() {
+		//if (transport.RoomName.Length != 6)
+		//	return;
+		NetworkManager.Singleton.StartClient();
+		buttons.SetActive(false);
+	}
+
+	public void Quit() {
+		Application.Quit();
+	}
+}
