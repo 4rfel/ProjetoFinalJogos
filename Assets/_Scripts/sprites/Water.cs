3932fab53fe61337da86a56a973bf496c5b8afe4
mariaeduardabicalho <mariaeduardabicalho@gmail.com>
2021-05-28 12:26:27 -0300

    sprint 2

---
+++ Assets/_Scripts/sprites/Water.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAPI;
+
+public class Water : NetworkBehaviour
+{
+    
+
+    private void OnCollisionEnter(Collision collision) {
+		//PlaySound("hit");
+		collision.gameObject.GetComponent<Rigidbody>().position= collision.gameObject.GetComponent<PlayerMovement>().prePosition;
+
+    }
+}
--- Assets/_Scripts/sprites/rotate.cs
+++ Assets/_Scripts/sprites/rotate.cs
@@ -7,7 +7,7 @@ public class rotate : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent < Rigidbody >().AddTorque(transform.forward * 1f,ForceMode.Acceleration);
+        GetComponent < Rigidbody >().AddTorque(transform.right * 100f,ForceMode.Acceleration);
         // transform.Rotate(new Vector3(0f,0f,1f));
     }
 
